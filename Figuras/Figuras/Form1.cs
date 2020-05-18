9e4d6ae2bbe6e910a270ba3105748258b7edc376
RogelioCR03 <60370979+RogelioCR03@users.noreply.github.com>
2020-05-18 11:44:09 -0700

    .

--- Figuras/Figuras/Form1.cs
+++ Figuras/Figuras/Form1.cs
@@ -12,9 +12,21 @@ namespace Figuras
 {
     public partial class Form1 : Form
     {
+        private Button miBoton;
         public Form1()
         {
             InitializeComponent();
+
+            this.Text = "Figuras";
+
+            this.miBoton = new System.Windows.Forms.Button();
+            this.miBoton.Location = new System.Drawing.Point(254, 272);
+            this.miBoton.Name = "button2";
+            this.miBoton.Size = new System.Drawing.Size(75, 23);
+            this.miBoton.TabIndex = 1;
+            this.miBoton.Text = "button2";
+            this.miBoton.UseVisualStyleBackColor = true;
+            this.Controls.Add(this.miBoton);
         }
     }
 }
